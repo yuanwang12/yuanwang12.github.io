634d99a16168d17f9c3d253bf2a91bbd93e298d1
yuanwang12 <15551550767@163.com>
2019-06-30 23:04:59 +0800

    通过state 组件动态通信

--- workspace/react-basic/src/App.js
+++ workspace/react-basic/src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
 
 class App extends React.Component {
+
+  // state
+  state = {
+    persons: [
+      { name: "汪渊1号", age: "18" },
+      { name: "汪渊2号", age: "20" },
+      { name: "汪渊3号", age: "22" }
+    ]
+  }
+
   render () {
     return (
       <div className="main">
         <h3>Hello World</h3>
-        <Person name="汪渊1号" age="18" />
-        <Person name="汪渊2号" age="20" />
-        <Person name="汪渊3号" age="22" />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     )
   }
