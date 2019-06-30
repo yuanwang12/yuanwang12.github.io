dc43c894829b4efb820031abcadfc31ba0b019b4
yuanwang12 <15551550767@163.com>
2019-06-30 21:52:27 +0800

    Person组件
    第一个组件Peson.js

---
+++ workspace/react-basic/src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Person.css'
+
+const Person = () => {
+  return (
+    <div className="personWrapp">
+      <h3>我是第一个Person组件</h3>
+    </div>
+  )
+}
+
+export default Person;
