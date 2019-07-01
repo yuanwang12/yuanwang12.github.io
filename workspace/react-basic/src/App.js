26caf68868e58f212460493ae9edfa55122123d8
yuanwang12 <15551550767@163.com>
2019-07-01 19:59:57 +0800

    删除person信息

--- workspace/react-basic/src/App.js
+++ workspace/react-basic/src/App.js
@@ -7,9 +7,9 @@ class App extends React.Component {
   // state
   state = {
     persons: [
-      { name: "汪渊1号", age: "18" },
-      { name: "汪渊2号", age: "20" },
-      { name: "汪渊3号", age: "22" }
+      { name: "汪渊1号", age: "18", id: "1" },
+      { name: "汪渊2号", age: "20", id: "2" },
+      { name: "汪渊3号", age: "22", id: "3" }
     ],
     personState: false
   }
@@ -18,9 +18,9 @@ class App extends React.Component {
   changePerson = () => {
     this.setState({
       persons: [
-        { name: "汪渊001号", age: "23" },
-        { name: "汪渊002号", age: "23" },
-        { name: "汪渊003号", age: "33" }
+        { name: "汪渊001号", age: "23", id: "1" },
+        { name: "汪渊002号", age: "23", id: "2" },
+        { name: "汪渊003号", age: "33", id: "3" }
       ]
     })
   }
@@ -30,9 +30,9 @@ class App extends React.Component {
     let newName = event.target.value;
     this.setState({
       persons: [
-        { name: newName, age: "23" },
-        { name: newName, age: "23" },
-        { name: newName, age: "33" }
+        { name: newName, age: "23", id: "1" },
+        { name: newName, age: "23", id: "2" },
+        { name: newName, age: "33", id: "3" }
       ]
     })
   }
@@ -49,24 +49,42 @@ class App extends React.Component {
     });
   }
 
-  render () {
+  // deletePerson
+  deletePerson = (event, id) => {
+    // 通过id查找当前id所在对象
+    let personIndex = this.state.persons.findIndex(obj => {
+      return obj.id === id;
+    })
+    let persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({
+      persons: persons
+    })
+  }
 
+  render () {
+    // 加载dom
     let person = null;
     if (this.state.personState) {
+      let persons = [...this.state.persons]
       person = (
-        this.state.persons.map(personObj => {
-          return <Person name={personObj.name} age={personObj.age} />
+        persons.map(personObj => {
+          console.log(personObj)
+          return <Person
+            key={personObj.age}
+            deleteThisObj={event => this.deletePerson(event, personObj.id)}
+            name={personObj.name}
+            age={personObj.age} />
         })
       )
     }
 
+    // 渲染dom
     return (
       <div className="main">
         <h3>Hello World</h3>
         <button onClick={this.toggleState}>切换状态</button>
         {person}
-
-
       </div>
     )
   }
